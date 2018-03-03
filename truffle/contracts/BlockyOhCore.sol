69624a27cbfdff0a6ee3d30543d0c8c2a9377c42
p.mihaylov <p.mihaylov@ocado.com>
2018-03-03 16:35:34 +0200

    modified blocky oh contract

--- truffle/contracts/BlockyOhCore.sol
+++ truffle/contracts/BlockyOhCore.sol
@@ -36,7 +36,7 @@ contract CardFactory is Ownable {
     Card[] public definedCards;
     mapping(address => uint[]) playerCards;
     mapping(address => uint) personWinsCount;
-
+    
     modifier bothPlayersRegistered(address player1, address player2) {
         require(playerCards[player1].length > 0);
         require(playerCards[player2].length > 0);
@@ -46,7 +46,7 @@ contract CardFactory is Ownable {
     function CardFactory() public {
         // genesis card
         definedCards.push(Card("", 0, 0, Rarity.Unique));
-
+        
         // starting deck
         definedCards.push(Card("LameWarrior", 10, 1, Rarity.Common));
         definedCards.push(Card("SemiLameWarrior", 7, 3, Rarity.Common));
@@ -57,12 +57,16 @@ contract CardFactory is Ownable {
 
     function register() public {
         require(playerCards[msg.sender].length == 0);
-
+        
         playerCards[msg.sender] = getStartingDeck();
     }
 
     function getCardsOf(address owner) public view returns (uint[]) {
         return playerCards[owner];
+    }
+    
+    function totalCardsCount() public view returns (uint) {
+        return definedCards.length;
     }
 
     function createCard(string name, uint8 attack, uint8 health, Rarity rarity) public onlyOwner returns (uint) {
@@ -81,7 +85,7 @@ contract CardFactory is Ownable {
 
         return uint(keccak256(seed)) % max;
     }
-
+    
     function playerCardCount(address player, uint cardId) internal view returns (uint) {
         uint count = 0;
         for (uint i = 0; i < playerCards[player].length; i++) {
@@ -89,7 +93,7 @@ contract CardFactory is Ownable {
                 count++;
             }
         }
-
+        
         return count;
     }
 
@@ -134,82 +138,86 @@ contract BlockyOhMarket is BlockyOhDuel {
         uint cardId;
         uint price;
     }
-
-    CardSale[] public sales;
-
+    
+    CardSale[] public cardSales;
+    
     function BlockyOhMarket() public {
         // genesis sale
-        sales.push(CardSale(0, 0, 0));
+        cardSales.push(CardSale(0, 0, 0));
     }
-
+    
+    function totalCardSalesCount() public view returns (uint) {
+        return cardSales.length;
+    }
+    
     function setCardForSale(uint cardId, uint price) public returns (uint) {
         require(cardId != 0);
         require(playerCardCount(msg.sender, cardId) > countOfCardIdSoldByPlayer(msg.sender, cardId));
-
-        sales.push(CardSale(msg.sender, cardId, price));
-
-        return sales.length - 1;
+        
+        cardSales.push(CardSale(msg.sender, cardId, price));
+        
+        return cardSales.length - 1;
     }
-
+    
     function removeCardSale(uint saleId) public {
-        require(saleId < sales.length);
-        require(sales[saleId].owner == msg.sender);
-
-        delete sales[saleId];
+        require(saleId < cardSales.length);
+        require(cardSales[saleId].owner == msg.sender);
+        
+        delete cardSales[saleId];
     }
-
+    
     function buyTradedCard(uint saleId) public payable {
-        require(saleId < sales.length);
-        require(sales[saleId].owner != address(0));
-        require(sales[saleId].owner != msg.sender);
-        require(sales[saleId].price == msg.value);
-
-        address saleOwner = sales[saleId].owner;
-        uint boughtCard = sales[saleId].cardId;
+        require(saleId < cardSales.length);
+        require(cardSales[saleId].owner != address(0));
+        require(cardSales[saleId].owner != msg.sender);
+        require(cardSales[saleId].price == msg.value);
+        
+        address saleOwner = cardSales[saleId].owner;
+        uint boughtCard = cardSales[saleId].cardId;
         uint[] storage cards = playerCards[saleOwner];
-
+        
         for (uint i = 0; i < cards.length; i++) {
-            if (cards[i] == sales[saleId].cardId) {
+            if (cards[i] == cardSales[saleId].cardId) {
                 delete cards[i];
             }
         }
-
+        
         playerCards[msg.sender].push(boughtCard);
-        delete sales[saleId];
-
+        delete cardSales[saleId];
+        
         saleOwner.transfer(msg.value);
     }
-
+    
     function salesByPlayer(address player, uint page) public view returns (uint[SALE_PAGE_SIZE]) {
         uint[SALE_PAGE_SIZE] memory playerSales;
         uint salesCount = 0;
         uint playerSalesIndex = 0;
-
-        for (uint saleIndex = 0; saleIndex < sales.length; saleIndex++) {
-            if (sales[saleIndex].owner == player) {
-                if (salesCount / SALE_PAGE_SIZE == page) {
+        
+        for (uint saleIndex = 0; saleIndex < cardSales.length; saleIndex++) {
+            if (cardSales[saleIndex].owner == player) {
+                if (salesCount / 2 == page) {
                     playerSales[playerSalesIndex++] = saleIndex;
                 }
-
+                
                 salesCount++;
             }
-
+            
             if (playerSalesIndex == SALE_PAGE_SIZE) {
                 break;
             }
         }
-
+        
         return playerSales;
     }
-
+    
     function countOfCardIdSoldByPlayer(address player, uint cardId) private view returns (uint) {
         uint count = 0;
-        for (uint i = 0; i < sales.length; i++) {
-            if (sales[i].owner == player && sales[i].cardId == cardId) {
+        for (uint i = 0; i < cardSales.length; i++) {
+            if (cardSales[i].owner == player && cardSales[i].cardId == cardId) {
                 count++;
             }
         }
-
+        
         return count;
     }
 }
