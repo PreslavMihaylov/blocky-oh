d901c7e8c2da62ca7a662055468776dffa1a706c
PreslavMihaylov <preslav@pmihaylov.com>
2018-03-09 21:26:40 +0200

    added duel oracle web app

---
+++ duel-oracle/index.js
@@ -0,0 +1,45 @@
+let newChallengeEvent = contract.NewChallenge();
+
+newChallengeEvent.watch(function(err, result) {
+    if (err) console.log("Smart contract error: " + err);
+
+    let challenger = result.args['challenger'];
+    let opponent = result.args['opponent'];
+    console.log(challenger);
+    console.log(opponent);
+    let hasWon = parseInt(Math.random() * 100) < 50;
+
+    let winner;
+    if (hasWon) {
+        winner = challenger;
+    } else {
+        winner = opponent;
+    }
+
+    contract.winsCountOf(winner, function(err, result) {
+        if (err) console.log("Smart contract error: " + err);
+
+        let winsCount = result.toNumber();
+        console.log(winsCount);
+
+        if (winsCount % 5 == 0) {
+            contract.totalCardsCount(function(err, result) {
+                if (err) console.log("Smart contract error: " + err);
+
+                let cardsCnt = result.toNumber();
+                let newCardId = (parseInt(Math.random() * 100) % (cardsCnt - 1)) + 1;
+                contract.settleDuel(challenger, opponent, hasWon, newCardId, function(err, result) {
+                    if (err) console.log("Smart contract error: " + err);
+
+                    console.log("duel settled");
+                });
+            });
+        } else {
+            contract.settleDuel(challenger, opponent, hasWon, 0, function(err, result) {
+                if (err) console.log("Smart contract error: " + err);
+
+                console.log("duel settled");
+            });
+        }
+    });
+});
